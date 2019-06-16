12c39dc91017dab8e9c24309942c7d7951a76075
Irvin Reyes <reyes.yerp@gmail.com>
2019-06-15 19:20:55 -0500

    Fix spinner width

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -6,7 +6,7 @@ const Spinner = () => (
     <img
       src={spinner}
       alt="Loading..."
-      style={{ width: "2000px", margin: "auto", display: "block" }}
+      style={{ width: "200px", margin: "auto", display: "block" }}
     />
   </Fragment>
 );
